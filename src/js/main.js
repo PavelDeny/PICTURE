2ed8f7373e93920a73c60d447790eb19f261882b
Pavel D. <paveldenysenko91@gmail.com>
2025-05-20 13:07:40 +0300

    added element loading from server and simple calculator

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
+import calc from "./modules/calc";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -20,5 +21,6 @@ window.addEventListener("DOMContentLoaded", () => {
   mask('[name="phone"]');
   checkTextInputs('[name="name"]');
   checkTextInputs('[name="message"]');
-  showMoreStyles(".button-styles", ".styles-2");
+  showMoreStyles(".button-styles", "#styles .row");
+  calc("#size", "#material", "#options", ".promocode", ".calc-price");
 });
