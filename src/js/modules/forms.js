31233c92db47d489af183d55174a932d7e6be6d9
Pavel D. <paveldenysenko91@gmail.com>
2025-05-21 12:05:13 +0300

    added d&d

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -30,15 +30,29 @@ const forms = () => {
 
   upload.forEach((item) => {
     item.addEventListener("input", () => {
-      console.log(item.files[0]);
-      let dots;
-      const arr = item.files[0].name.split(".");
-
-      arr[0].length > 6 ? (dots = "...") : (dots = ".");
-      const name = arr[0].substring(0, 6) + dots + arr[1];
-      item.previousElementSibling.textContent = name;
+      if (item.files.length > 0) {
+        let dots;
+        const arr = item.files[0].name.split(".");
+        arr[0].length > 6 ? (dots = "...") : (dots = ".");
+        const name = arr[0].substring(0, 6) + dots + arr[1];
+        item.previousElementSibling.textContent = name;
+      } else {
+        item.previousElementSibling.textContent = "Файл не выбран";
+      }
     });
   });
+
+  //  upload.forEach((item) => {
+  //    item.addEventListener("input", () => {
+  //      console.log(item.files[0]);
+  //      let dots;
+  //      const arr = item.files[0].name.split(".");
+
+  //      arr[0].length > 6 ? (dots = "...") : (dots = ".");
+  //      const name = arr[0].substring(0, 6) + dots + arr[1];
+  //      item.previousElementSibling.textContent = name;
+  //    });
+  //  });
 
   form.forEach((item) => {
     item.addEventListener("submit", (e) => {
